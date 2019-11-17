082b9718998a426bd61fed7edae87e621401e1a1
Ian Turner <ianturner88@gmail.com>
2019-11-16 21:31:43 -0500

    Intro to Data Types

---
+++ js_sandbox/datatypes.js
@@ -0,0 +1,29 @@
+//primitive
+
+//string 
+const name = 'John Doe';
+//number
+const age = 45;
+//boolean
+const hasKids = true;
+//null
+const car = null;
+//undefined
+let test;
+//symbol
+const sym = Symbol();
+
+console.log(typeof sym);
+
+//Reference Types - Objects
+//array
+const hobbies = ['movies', 'music'];
+//object literal
+const address = {
+    city: 'Boston',
+    state: 'MA'
+}
+
+const today = new Data();
+console.log(today);
+console.log(typeof hobbies);
