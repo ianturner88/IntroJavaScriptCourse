61cc973efb95ab35487e71efbe25bfde970e3702
Ian Turner <ianturner88@gmail.com>
2019-11-18 18:52:54 -0500

    End of Lesson 9 (Numbers & the Math Object)

---
+++ js_sandbox/numbers.js
@@ -0,0 +1,54 @@
+const num1 = 100;
+const num2 = 50;
+let val;
+
+//simple math with numbers
+val = num1 + num2;
+console.log(val);
+
+val = num1 * num2;
+console.log(val);
+
+val = num1 - num2;
+console.log(val);
+
+val = num1 / num2;
+console.log(val);
+
+val = num1 % num2;
+console.log(val);
+
+//math objects
+val = Math.PI;
+console.log(val);
+
+val = Math.E;
+console.log(val);
+
+val = Math.round(2.4);
+console.log(val);
+
+val = Math.ceil(2.4);
+console.log(val);
+
+val = Math.floor(2.8);
+console.log(val);
+
+val = Math.sqrt(64);
+console.log(val);
+
+val = Math.sqrt(64);
+console.log(val);
+
+val = Math.sqrt(64);
+console.log(val);
+
+val = Math.abs(-3);
+console.log(val);
+
+val = Math.pow(8,2);
+console.log(val);
+
+//a random number between 1 and 20
+val = Math.floor(Math.random() * 20 + 1);
+console.log(val);
