3dc249062958dc5dd67d48c35cc4bcff41708459
Ian Turner <ianturner88@gmail.com>
2020-01-12 22:06:11 -0500

    Lesson 44 complete!

--- Section4/L44.js
+++ Section4/L44.js
@@ -1,7 +1,19 @@
-const brad = {
-    name: 'Brad',
-    age: 30
+// Person Constructor
+function Person(name, dob) {
+    this.name = name;
+    this.birthday = new Date();
+    this.calculateAge = function(){
+        const diff = Date.now() - this.birthday.getTime()
+        const ageDate = new Date(diff);
+        return Math.abs(ageDate.getUTCFullYear() - 1970);
+    }
 }
 
-console.log(brad);
-console.log(brad.age);
+// const brad = new Person('Brad');
+// const john = new Person('John');
+
+// console.log(brad);
+// console.log(john);
+
+const brad = new Person('Brad', '9-10-1981');
+console.log(brad.calculateAge());
