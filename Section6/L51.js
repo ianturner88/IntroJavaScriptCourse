3685192563f8efc37014c34d4c12e006423b91be
Ian Turner <ianturner88@gmail.com>
2020-01-16 12:42:18 -0500

    Lesson 53

--- Section6/L51.js
+++ Section6/L51.js
@@ -23,6 +23,26 @@ UI.prototype.addBookToList = function(book) {
 
     list.appendChild(row);
 
+    // Show alert
+    UI.prototype.showAlert = function(message, className){
+        // Create div
+        const div = document.createElement('div');
+        // Add classes
+        div.className = `alert ${className}`;
+        // Add text
+        div.appendChild(document.createTextNode(message));
+        // Get parent
+        const container = document.querySelector('container');
+        const form = document.querySelector('#book-form');
+        // Insert alert
+        container.insertBefore(div, form);
+
+        // Timeout after 3 seconds
+        setTimeout(function() {
+            document.querySelector('.alert').remove();
+        }, 3000);
+    }
+
     // Clear fields
     UI.prototype.clearFields = function() {
         document.getElementById('title').value = '';
@@ -41,18 +61,28 @@ document.getElementById('book-form').addEventListener('submit', function(e){
 
     // instantiate book
     const book = new Book(title, author, isbn);
-
-    // instantiate UI
+    // Instantiate UI
     const ui = new UI();
-
     console.log(ui);
 
-    // add book to list
+    // Add book to list
     ui.addBookToList(book);
+
+    // Validate
+    if(title === "" || author === "" || isbn === ""){
+        // Error alert
+        UI.showAlert('Please fill in all fields', "error");
+    } else {
+        // Add book to list
+        ui.addBookToList(book);
+        // Show success
+        ui.showAlert('Book Added', 'success');
+        // Clear fields
+        ui.clearFields();
+    }
 
     // Clear fields
     ui.clearFields();
-
     // console.log(book);
 
     e.preventDefault();
