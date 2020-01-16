dc147a982eebad50e74f5604cd36240282c5cb88
Ian Turner <ianturner88@gmail.com>
2020-01-16 16:57:06 -0500

    L55

--- Section6/L55.js
+++ Section6/L55.js
@@ -1,31 +1,30 @@
-// Book Constructor
-function Book(title, author, isbn) {
-    this.title = title;
-    this.author = author;
-    this.isbn = isbn;
+class Book{
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
 }
 
-// UI Constructor
-function UI() {}
+class UI {
+    addBookToList(book) {
 
-// Add Book to list
-UI.prototype.addBookToList = function(book) {
-    const list = document.getElementById('book-list');
-    // Create tr element
-    const row = document.createElement('tr');
-    // Insert columns
-    row.innerHTML = `
-        <td>${book.title}</td>
-        <td>${book.author}</td>
-        <td>${book.isbn}</td>
-        <td><a href="#" class="delete">X<a></td>
+        const list = document.getElementById('book-list');
+            // Create tr element
+            const row = document.createElement('tr');
+            // Insert columns
+            row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+            <td><a href="#" class="delete">X<a></td>
     `;
 
     list.appendChild(row);
 
-    // Show alert
-    UI.prototype.showAlert = function(message, className){
-        // Create div
+    }
+
+    showAlert(message, className) {
         const div = document.createElement('div');
         // Add classes
         div.className = `alert ${className}`;
@@ -43,24 +42,20 @@ UI.prototype.addBookToList = function(book) {
         }, 3000);
     }
 
-    //Delete book
-    ui.prototype.deleteBook = function(target) {
-        if(target.className === 'delete') {
+    deleteBook(target) {
+        if (target.className === 'delete') {
             target.parentElement.parentElement.remove();
         }
     }
 
-    // Clear fields
-    UI.prototype.clearFields = function() {
+    clearFields() {
         document.getElementById('title').value = '';
         document.getElementById('author').value = '';
         document.getElementById('isbn').value = '';
     }
-
-    console.log(row);
 }
 
-// Event listener for add book
+// Event listeners
 document.getElementById('book-form').addEventListener('submit', function(e){
     console.log('test');
 
@@ -91,21 +86,6 @@ document.getElementById('book-form').addEventListener('submit', function(e){
     // Clear fields
     ui.clearFields();
     // console.log(book);
-
-    e.preventDefault();
-});
-
-// Event listener for delete
-document.getElementById('book-list').addEventListener('click', function(e){
-    
-    // Instantiate UI
-    const ui = new UI();
-
-    // Delete book
-    ui.deleteBook(e.target);
-
-    // Show message
-    ui.showAlert('Book Removed!', 'success');
 
     e.preventDefault();
 });
